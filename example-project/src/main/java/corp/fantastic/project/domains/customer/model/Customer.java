3f95eb856d869e480de2832a2874618b20c8d0f5
Oliver B. Fischer <oliver.fischer@freiheitsgrade.consulting>
2022-09-06 10:41:46 +0000

    Validierung auf Zugriffsverletzungen innerhalb der Layer
    Das Beispiel wurde jetzt um Regeln erweitert, über die auf nicht
    erlaubte Zugriff zwischen Adapter und Domains hin validiert wird.
    
    In diesem Beispiel ist der Zugriff von Services aufeinander erlaubt.

--- example-project/src/main/java/corp/fantastic/project/domains/customer/model/Customer.java
+++ example-project/src/main/java/corp/fantastic/project/domains/customer/model/Customer.java
@@ -1,11 +1,13 @@
 package corp.fantastic.project.domains.customer.model;
 
 import corp.fantastic.project.domains.scoring.model.RiscScore;
+import corp.fantastic.project.domains.scoring.model.AlternativeRiscScore;
 import java.util.UUID;
 
 public class Customer {
     private String name;
     private RiscScore riscScore;
+    private AlternativeRiscScore riscScore2;
     private UUID id;
 
     private CustomerStatus status;
@@ -24,6 +26,13 @@ public class Customer {
 
     public void setRiscScore(RiscScore riscScore) {
         this.riscScore = riscScore;
+    }
+    public AlternativeRiscScore getRiscScore2() {
+        return riscScore2;
+    }
+
+    public void setRiscScore2(AlternativeRiscScore riscScore) {
+        this.riscScore2 = riscScore;
     }
 
     public UUID getId() {
---
+++ example-project/src/main/java/corp/fantastic/project/domains/scoring/model/AlternativeRiscScore.java
@@ -0,0 +1,8 @@
+package corp.fantastic.project.domains.scoring.model;
+
+public enum AlternativeRiscScore {
+    UNKNOWN,
+    HIGH,
+    LOW,
+    NOT_ACCEPTABLE;
+}
